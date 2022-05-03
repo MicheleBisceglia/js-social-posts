26ad63537550296f3a025858305b73d4c63afad1
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-05-03 14:49:02 +0200

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Milestone 1 - Creiamo il nostro array di oggetti
 const posts = [
     {
         "id": 1,
@@ -8,6 +9,51 @@ const posts = [
             "image": "https://unsplash.it/300/300?image=15"
         },
         "likes": 80,
-        "created": "2021-06-25"
+        "created": "25-04-2022"
+    },
+    {
+        "id": 2,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Jocopoco Majoco",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 23,
+        "created": "25-04-2022"
+    },
+    {
+        "id": 3,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "joe Jò",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 115,
+        "created": "23-04-2022"
+    },
+    {
+        "id": 4,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Giovanna Lamanna",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 567,
+        "created": "15-04-2022"
+    },
+    {
+        "id": 5,
+        "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+        "media": "https://unsplash.it/600/300?image=171",
+        "author": {
+            "name": "Ilaria Condizionata",
+            "image": "https://unsplash.it/300/300?image=15"
+        },
+        "likes": 345,
+        "created": "13-04-2022"
     }
 ];
+console.log(posts)
